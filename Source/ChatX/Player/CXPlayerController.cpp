6c8f52c7897b53b38415df85b7d69a1541f496a5
saltlake00 <saltlake5470@gmail.com>
2026-03-13 14:26:35 +0900

    feat: 1-2 ~ 1-4

--- Source/ChatX/Player/CXPlayerController.cpp
+++ Source/ChatX/Player/CXPlayerController.cpp
@@ -3,12 +3,18 @@
 
 #include "Player/CXPlayerController.h"
 
+#include "ChatX.h"
 #include "UI/CXChatInput.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 void ACXPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (IsLocalController() == false)
+	{
+		return;
+	}
 
 	FInputModeUIOnly InputModeUIOnly;
 	SetInputMode(InputModeUIOnly);
@@ -31,11 +37,19 @@ void ACXPlayerController::BeginPlay()
 void ACXPlayerController::SetChatMessageString(const FString& InChatMessageString)
 {
 	ChatMessageString = InChatMessageString;
-	
+
 	PrintChatMessageString(ChatMessageString);
 }
 
 void ACXPlayerController::PrintChatMessageString(const FString& InChatMessageString)
 {
-	UKismetSystemLibrary::PrintString(this, ChatMessageString, true, true, FLinearColor::Red, 5.0f);
+	//UKismetSystemLibrary::PrintString(this, ChatMessageString, true, true, FLinearColor::Red, 5.0f);
+
+	/*
+	FString NetModeString = ChatXFunctionLibrary::GetNetModeString(this);
+	FString CombinedMessageString = FString::Printf(TEXT("%s, %s"), *NetModeString, *InChatMessageString);
+	ChatXFunctionLibrary::MyPrintString(this, CombinedMessageString, 10.f);
+	*/
+	
+	ChatXFunctionLibrary::MyPrintString(this, InChatMessageString, 15.f);
 }
